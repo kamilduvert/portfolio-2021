dba5eba4bec8815418bd36d29d88d8460f9ab112
Kamil Duvert <kduvert@gmail.com>
2021-04-29 12:19:40 +0200

    add_google_analytics

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,31 @@
+import { useEffect } from 'react';
+import { withRouter } from 'react-router-dom'
+import ReactGA from "react-ga";
+
 import Hero from "../Hero";
 import About from "../About";
 import Projects from "../Projects";
 import Contact from "../Contact";
 import Footer from "../Footer";
 
+const TRACKING_ID = process.env.REACT_APP_GA_TRAKING_ID;
+ReactGA.initialize(TRACKING_ID);
+
 const App = () => {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  })
+
   return (
-  <div className="app">
-    <Hero />
-    <About />
-    <Projects />
-    <Contact />
-    <Footer />
-  </div>
+    <div className="app">
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
   );
 };
 
-export default App;
+export default withRouter(App);
